fe023b9091edd653a23b8d3a25b4657b6e33b017
alocke12992 <alocke12992@gmail.com>
2018-07-03 13:17:55 -0600

    Creates index route

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,5 +1,8 @@
 class Api::ContactsController < ApplicationController
+  before_action :set_contact, only: [:show, :update, :destroy]
+
   def index
+    render json: Contact.all
   end
 
   def show
@@ -12,5 +15,22 @@ class Api::ContactsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  def contact_params
+    params.require(:contact).permit(
+      :first_name,
+      :last_name, 
+      :email,
+      :phone,
+      :address,
+      :avatar
+    )
   end
 end
