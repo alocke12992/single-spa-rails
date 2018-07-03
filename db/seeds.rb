b11ad6c0333c7be96408593600e830979b9a3b9c
alocke12992 <alocke12992@gmail.com>
2018-07-03 13:08:00 -0600

    Creates contacts and seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do 
+  Contact.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.free_email,
+    phone: Faker::PhoneNumber.phone_number,
+    address: Faker::Address.full_address,
+    avatar: Faker::Avatar.image,
+  )
+  end 
+
+  puts "Ten Contacts have been created"
